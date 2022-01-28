3cc8ca1fdf52878d15d5ccd14b956a15f2a7f4da
Prachi2002Aggarwal <prachi2002aggarwal@gmail.com>
2022-01-29 00:13:21 +0530

    DSA

--- Hashing/IntersectionOf2Arrays/IntersectionOf2Arrays.cpp
+++ Hashing/IntersectionOf2Arrays/IntersectionOf2Arrays.cpp
@@ -1,4 +1,5 @@
-/// My solution - wrong
+
+/// ES 1
 /*#include<iostream>
 using namespace std;
 #include<unordered_map>
@@ -6,49 +7,6 @@ using namespace std;
 int NumberofElementsInIntersection(int arr1[] , int arr2[] , int n1 , int n2) {
     unordered_set<int> u1(arr1 , arr1+n1);
     unordered_set<int> u2(arr2 , arr2+n2);
-    unordered_map<int , int > u;
-    for(int i = 0 ; i < n1 ; i++) {
-        u[arr1[i]]++;
-    }
-    for(int i = 0 ; i < n2 ; i++) {
-        u[arr2[i]]++;
-    }
-    int count = 0;
-    for(auto x : u) {
-        if(x.second > 1) {
-            count++;
-        }
-    }
-    return count;
-}
-int main() {
-    int n1 ;
-    cin >> n1;
-    int arr1[n1];
-    for(int i = 0 ; i < n1 ; i++) {
-        cin >> arr1[i];
-    }
-
-    int n2 ;
-    cin >> n2;
-    int arr2[n2];
-    for(int i = 0 ; i < n2 ; i++) {
-        cin >> arr2[i];
-    }
-    int ans = NumberofElementsInIntersection(arr1 , arr2 , n1 , n2);
-    cout << ans;
-}*/
-
-// ES 1
-/*
-#include<iostream>
-using namespace std;
-#include<unordered_map>
-#include<unordered_set>
-int NumberofElementsInIntersection(int arr1[] , int arr2[] , int n1 , int n2) {
-    unordered_set<int> u1(arr1 , arr1+n1);
-    unordered_set<int> u2(arr2 , arr2+n2);
-    unordered_map<int , int > u;
     int count = 0;
     for(auto x : u1) {
         if(u2.count(x)) {
@@ -75,19 +33,17 @@ int main() {
     cout << ans;
 }*/
 
-// ES 2
-#include<iostream>
+/// ES 2
+/*#include<iostream>
 using namespace std;
-#include<unordered_map>
 #include<unordered_set>
 int NumberofElementsInIntersection(int arr1[] , int arr2[] , int n1 , int n2) {
     unordered_set<int> u1(arr1 , arr1+n1);
-    unordered_map<int , int > u;
     int count = 0;
-    for(auto x : u1) {
-        if(arr2[x]) {
+    for(int i = 0 ; i < n2 ; i++) {
+        if(u1.count(arr2[i])) {
             count++;
-            u1.erase(x);
+            u1.erase(arr2[i]);
         }
     }
     return count;
@@ -108,4 +64,4 @@ int main() {
     }
     int ans = NumberofElementsInIntersection(arr1 , arr2 , n1 , n2);
     cout << ans;
-}
+}*/
