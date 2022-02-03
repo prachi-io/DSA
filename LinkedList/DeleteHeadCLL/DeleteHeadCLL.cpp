d095c5eade9959d429d330e970c268901fb35728
Prachi2002Aggarwal <prachi2002aggarwal@gmail.com>
2022-02-03 13:54:35 +0530

    DSA

--- LinkedList/DeleteHeadCLL/DeleteHeadCLL.cpp
+++ LinkedList/DeleteHeadCLL/DeleteHeadCLL.cpp
@@ -28,3 +28,76 @@ int main() {
     head = DeleteHead(head);
     return 0;
 }*/
+
+/// Sir - Naive
+/*
+#include<iostream>
+using namespace std;
+struct Node {
+    int data;
+    Node *next;
+    Node (int x) {
+        data = x;
+        next = NULL;
+    }
+};
+Node *DeleteHead(Node *head) {
+    if (head == NULL) {
+        return NULL;
+    }
+    if(head -> next == head) {
+        delete head;
+        return NULL;
+    }
+    Node *curr = head;
+    while(curr -> next != head) {
+         curr = curr -> next;
+    }
+    curr -> next = head -> next;
+    delete head;
+    return curr -> next;
+}
+int main() {
+    Node *head = new Node(10);
+    head -> next = new Node(5);
+    head -> next -> next = new Node(20);
+    head -> next -> next -> next = new Node(15);
+    head -> next -> next -> next -> next = head;
+    head = DeleteHead(head);
+    return 0;
+}*/
+
+/// Sir - Efficient
+#include<iostream>
+using namespace std;
+struct Node {
+    int data;
+    Node *next;
+    Node (int x) {
+        data = x;
+        next = NULL;
+    }
+};
+Node *DeleteHead(Node *head) {
+    if (head == NULL) {
+        return NULL;
+    }
+    if(head -> next == head) {
+        delete head;
+        return NULL;
+    }
+    head -> data = head -> next -> data;
+    Node *temp = head -> next;
+    head -> next = head -> next -> next;
+    delete temp;
+    return head;
+}
+int main() {
+    Node *head = new Node(10);
+    head -> next = new Node(5);
+    head -> next -> next = new Node(20);
+    head -> next -> next -> next = new Node(15);
+    head -> next -> next -> next -> next = head;
+    head = DeleteHead(head);
+    return 0;
+}
--- LinkedList/DeleteKthCLL/DeleteKthCLL.cpp
+++ LinkedList/DeleteKthCLL/DeleteKthCLL.cpp
@@ -1,3 +1,5 @@
+/// My
+/*
 #include<iostream>
 using namespace std;
 struct Node {
@@ -25,6 +27,53 @@ Node *deleteKth(Node *head , int K) {
         return t;
     }
     return head;
+}
+int main() {
+    Node *head = new Node(10);
+    head -> next = new Node(20);
+    head -> next -> next = new Node(30);
+    head -> next -> next  -> next = head;
+    head = deleteKth(head , 2);
+    return 0;
+}*/
+
+/// Sir
+#include<iostream>
+using namespace std;
+struct Node {
+    int data;
+    Node *next;
+    Node (int x) {
+        data = x;
+        next = NULL;
+    }
+};
+Node *DeleteHead(Node *head) {
+    if (head == NULL) {
+        return NULL;
+    }
+    if(head -> next == head) {
+        delete head;
+        return NULL;
+    }
+    head -> data = head -> next -> data;
+    Node *temp = head -> next;
+    head -> next = head -> next -> next;
+    delete temp;
+    return head;
+}
+Node *deleteKth(Node *head , int K) {
+    if (head == NULL) {
+        return head;
+    }
+    if(K == 1) {
+        return DeleteHead(head);
+    }
+    Node *curr = head;
+    for (int i = 0 ; i < K - 2 ; i++) {
+        curr = curr -> next;
+    }
+    Node *
 }
 int main() {
     Node *head = new Node(10);
