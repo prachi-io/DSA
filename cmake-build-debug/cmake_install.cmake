657073fd331b1ba04c25e32621e7c886b141acf9
Prachi2002Aggarwal <prachi2002aggarwal@gmail.com>
2022-02-08 19:08:47 +0530

    DSA

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -292,6 +292,16 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("D:/ClionProjects/DSA/Queue/LinkedListImplementation/LinkedListImplementationOutput/cmake_install.cmake")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/ClionProjects/DSA/Queue/QueueTraversal/QueueTraversalOutput/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/ClionProjects/DSA/Queue/StackUsingQueue/StackUsingQueueOutput/cmake_install.cmake")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
